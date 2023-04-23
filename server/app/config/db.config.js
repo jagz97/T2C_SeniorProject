1445ee993c0f2e339b3a597f0c18c9ab38048928
jagz97 <68725607+jagz97@users.noreply.github.com>
2023-04-23 02:26:03 -0700

    updated back to local sql server

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "t2c-testdb.mysql.database.azure.com",
-    USER: "jagz97",
-    PASSWORD: "Singhj97!",
-    DB: "testdb",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "new_password",
+  DB: "testdb",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
