5be3af2b76a7559d6b5f72f6e45a0a58125c6901
33JaredL <jmarc.lagman@gmail.com>
2023-08-07 14:50:07 -0700

    Update icons to use react-icons

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,6 @@
-import React, {useState,useRef,useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useRef, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import './Login.css'
-import deleteIcon from '../../images/email-delete-icon.png'
-import hideIcon from '../../images/hide-password-icon.png'
-import googleIcon from '../../images/Google.png'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
@@ -12,10 +9,15 @@ import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 import InputGroup from 'react-bootstrap/InputGroup'
 
+import { FaRegCircleXmark, FaRegEyeSlash, FaRegEye } from 'react-icons/fa6'
+import { FcGoogle } from 'react-icons/fc'
+
+const REQUEST_URL = 'http://localhost:8080/api/users/auth/signin'
+
 const Login = () => {
 
-    const [emailData, setEmailData] = useState('adam')
-    const [passwordData, setPasswordData] = useState('ada123')
+    const [emailData, setEmailData] = useState('')
+    const [passwordData, setPasswordData] = useState('')
 
     // State for toggling pwd input visibility
     const [showPassword, setShowPassword] = useState(false)
@@ -33,23 +35,24 @@ const Login = () => {
     }
 
     // For email input text reset   
-   useEffect(() => {
-      emailInputRef.current.focus();
-   }, [emailData]);
+    useEffect(() => {
+        emailInputRef.current.focus();
+    }, [emailData]);
 
     // onclick handler for email text reset
     const resetText = () => setEmailData('')
-    
+
     // onclick handler for revealing pwd text
     const toggleHidden = () => setShowPassword(!showPassword)
-    
+
     // An onSubmit event handler which for now logs
     // the captured form data to the console
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault()
-        // console.log(emailData,passwordData)
-        const formData = {email: emailData, password: passwordData}
-        fetch("http://localhost:8080/api/users/auth/signin", {
+       
+        const formData = { email: emailData, password: passwordData }
+       
+        fetch(REQUEST_URL, {
             method: "POST",
             headers: {
                 "Accept": "application/json",
@@ -68,10 +71,11 @@ const Login = () => {
             if(errorMessage) {
                 setErrorMessage("")
             }
+            // Link to Home Page Route Here
         })
         .catch(err => {
-            err.json().then((json) => {
-                setErrorMessage(json.message)
+            err.json().then(({message}) => {
+                setErrorMessage(message)
             })
         })
     }
@@ -79,62 +83,62 @@ const Login = () => {
     return (
         <Container className='container-login-page'>
             <Row>
-               <Col className='d-flex justify-content-center'>
+                <Col className='d-flex justify-content-center'>
                     <div className='page-title-text'>Sign in to <strong>Travel2Connect</strong></div>
-               </Col>
-               
+                </Col>
+
                 <Col className='d-flex justify-content-center'>
                     <Card className="container-card">
                         <Card.Body className='p-0'>
                             <Form className='container-form' onSubmit={handleSubmit} >
-                                    <Form.Group>
-                                        <InputGroup className='container-email-input'>
-                                            <Form.Control 
-                                                type='text'
-                                                placeholder='Enter Email' 
-                                                value={emailData}
-                                                onChange={handleEmailData}
-                                                required
-                                                className='login-input'
-                                                ref={emailInputRef}
-                                            />
-                                            <InputGroup.Text className='login-input-addon' onClick={resetText}>
-                                                <img src={deleteIcon} alt="delete icon"></img>
-                                            </InputGroup.Text>
-                                        </InputGroup>
-                                    </Form.Group>
+                                <Form.Group>
+                                    <InputGroup className='container-email-input'>
+                                        <Form.Control
+                                            type='text'
+                                            placeholder='Enter Email'
+                                            value={emailData}
+                                            onChange={handleEmailData}
+                                            required
+                                            className='login-input'
+                                            ref={emailInputRef}
+                                        />
+                                        <InputGroup.Text className='login-input-addon' onClick={resetText}>
+                                            <FaRegCircleXmark/>
+                                        </InputGroup.Text>
+                                    </InputGroup>
+                                </Form.Group>
 
-                                    <Form.Group>
-                                        <InputGroup className='container-password-input'>                                    
-                                            <Form.Control 
-                                                type={ showPassword ? 'text': 'password'} 
-                                                placeholder='Enter Password' 
-                                                value={passwordData}
-                                                onChange={handlePasswordData}
-                                                required
-                                                className='login-input'
-                                            />
-                                            <InputGroup.Text className='login-input-addon' onClick={toggleHidden}>
-                                                <img src={hideIcon} alt="hide icon"></img>
-                                            </InputGroup.Text>
-                                            
-                                        </InputGroup>
-                                        <div className="login-error-message">{errorMessage}</div>
-                                        <div className='container-recover-password'>
-                                            <Link to='/' className='recover-password-btn'>Recover Password?</Link>
-                                        </div>
-                                      
-                                    </Form.Group>
-                                    
-                                    <Button className='btn-submit-login' type='submit'>Sign In</Button>
+                                <Form.Group>
+                                    <InputGroup className='container-password-input'>
+                                        <Form.Control
+                                            type={showPassword ? 'text' : 'password'}
+                                            placeholder='Enter Password'
+                                            value={passwordData}
+                                            onChange={handlePasswordData}
+                                            required
+                                            className='login-input'
+                                        />
+                                        <InputGroup.Text className='login-input-addon' onClick={toggleHidden}>
+                                            {showPassword ? <FaRegEyeSlash/> : <FaRegEye/>}
+                                        </InputGroup.Text>
+
+                                    </InputGroup>
+                                    <div className="login-error-message">{errorMessage}</div>
+                                    <div className='container-recover-password'>
+                                        <Link to='#' className='recover-password-btn'>Recover Password?</Link>
+                                    </div>
+
+                                </Form.Group>
+
+                                <Button className='btn-submit-login' type='submit'>Sign In</Button>
                             </Form>
 
                             <div className='container-continue'>
-                                <hr className='line-continue'/>
+                                <hr className='line-continue' />
                                 <div className='continue-text'>Or continue with</div>
-                                <hr className='line-continue'/>
+                                <hr className='line-continue' />
                             </div>
-                            <Link to='/' className='google-login'><img src={googleIcon} alt='google icon'/></Link>
+                            <Link to='#' className='google-login'><FcGoogle size={30}/></Link>
 
                         </Card.Body>
                     </Card>
