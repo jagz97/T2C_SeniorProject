15d4a6a66d8b6da8ed6c42a33e9db4950f0feebc
33JaredL <jmarc.lagman@gmail.com>
2023-10-12 18:02:07 -0700

    Add onboardprofilepic component

--- client/src/components/onboarding/OnboardProfilePic.js
+++ client/src/components/onboarding/OnboardProfilePic.js
@@ -1,14 +1,82 @@
-import React from "react"
-import Container from "react-bootstrap/esm/Container"
-import Row from 'react-bootstrap/Row'
+import React, { useState } from "react"
+import Container from 'react-bootstrap/Container'
+import Row from "react-bootstrap/Row"
 import Avatar from "../avatar/Avatar"
-import api from "../../api/axios"
+import "./OnboardProfilePic.css"
+import useAuth from "../../hooks/useAuth"
 
-const OnboardProfilePic = () => {
+import {
+    Plus
+} from "akar-icons"
+
+import { api } from "../../api/axios"
+
+const OnboardProfilePic = ({setCurrentPage}) => {
+
+    const [ usrImg, setUsrImg ] = useState("")
+    const [ errorMessage, setErrorMessage ] = useState("")
+
+    const { user } = useAuth()
+    
+    const submitHandler = async (event) => {
+        event.preventDefault()
+        const headerOptions = {
+            headers: {
+                Authorization: `${user.accesstoken}`,
+            }
+        }
+
+        try {
+            const request = api.post("/profile/updateProfilePic", usrImg, headerOptions)
+            console.log(request)
+        } catch (error) {
+            const errorMessage = error.response?.data?.message
+            // if we get a an error response from server display it
+            // otherwise we display error directly from axios library
+            if(errorMessage) {
+                setErrorMessage(errorMessage)
+            }
+            else {
+                setErrorMessage(error.message)
+            }
+        }
+    }
+
+    const handleUpload = (event) => {
+        const { files } = event.target
+        if(files.length > 0) {
+            if(files[0].type != "image/jpeg") {
+                setErrorMessage("File must be an image")
+            }
+            console.log(files[0])
+            setUsrImg(URL.createObjectURL(files[0]))
+        }
+    }
+    
+    console.log(usrImg)
+
     return (
         <Container>
             <Row className="justify-content-center align-items-center vh-100">
-                <Avatar size={250} color={"#2a2a2a"}/>
+                <form className="onboard-pfp-form" onSubmit={submitHandler}>
+                    <h1 className="onboard-pfp-header">Choose a profile picture</h1>
+                    <label htmlFor="onboard-file-input">
+                        <Avatar 
+                            src={usrImg} 
+                            size={275} 
+                            color={"#2a2a2a"}
+                            alt="user profile picture"
+                        />
+                        <div className="onboard-pfp-icon"><Plus size={15}/></div>
+                        </label>
+                    { errorMessage ? <p className="error">{errorMessage}</p>: null}
+                    <input 
+                        type="file" 
+                        name="usrImg"
+                        onChange={handleUpload}
+                        id="onboard-file-input"/>
+                    <button type="submit">Continue</button>
+                </form>
             </Row>
         </Container>
     )
