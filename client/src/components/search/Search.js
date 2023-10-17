c06dbe1b1916e64dea7010a48ac67404eb5904d1
33JaredL <jmarc.lagman@gmail.com>
2023-10-16 21:16:18 -0700

    Add Form Inputs

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -5,41 +5,24 @@ import SearchResult from './SearchResult'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import Navbar from '../navbar/Navbar'
 import introImg from '../../images/pexels-jake-brown-2531314.jpg'
 
 
 
 const Search = () => {
-
     return (
-        <div>
-            <Navbar/>
-            <Container fluid >
-                <Row className="container-intro-img position-relative">
-                    <img src={introImg} alt="sunset" className="intro-img"/>
+            <Container fluid>
+                <Row>
                     <h1 className="intro-text">Explore Places</h1>
+                    <img src={introImg} alt="sunset" className="intro-img"/>
                 </Row>
                 <Row style={{marginTop: "-65px"}}>
-                    <Col className="col-12 col-lg-9 mx-auto ">
+                    <Col className="col-auto col-sm-9 col-xl-12 mx-auto">
                         <SearchForm/>
                     </Col>
                 </Row>
-                <Row className="justify-content-center mb-1 px-0" style={{marginTop: "35px"}}>
-                  
-                    <SearchResult/>
-                    <SearchResult/>
-                    <SearchResult/>
-                    {/* <SearchResult/>
-                    <SearchResult/>
-                    <SearchResult/> */}
-                    
-                    
-                </Row>
             </Container>
-        </div>
     )
 }
-
 
 export default Search
--- client/src/components/search/SearchForm.js
+++ client/src/components/search/SearchForm.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Search.css'
+
+import {
+    FaLocationDot
+} from 'react-icons/fa6'
+
 
 const SearchForm = () => {
+
+    const [ location, setLocation ] = useState("")
+    const [ arrival, setArrival ] = useState("")
+    const [ departure, setDeparture ] = useState("")
+
+    console.log("loc", location)
+    console.log("arr,", arrival)
+    console.log("dep", departure)
+
     return (
         <div className="container-search-form">
-            <form>
-                <h1>Search Form</h1>
-            </form>
+            <div className="search-form-wrapper">
+                <form className="search-form d-sm-flex justify-content-between">
+                    <div className="search-location-wrapper">
+                        <label htmlFor="search-location">Location</label>
+                        <div className="position-relative">
+                            <span className="search-location-icon">
+                                <FaLocationDot size={12}/>
+                            </span>
+                            <input 
+                                type="text"
+                                value={location}
+                                name="location"
+                                placeholder="Where are you going?"
+                                onChange={(event) => setLocation(event.target.value)}
+                                id="search-location"
+                            />
+                        </div>
+                    </div>
+                    <div className="search-date-wrapper">
+                        <label htmlFor="search-date-arrival">Date Arrival</label>
+                        <input 
+                            type="date"
+                            value={arrival}
+                            name="arrival"
+                            onChange={(event) => setArrival(event.target.value)}
+                            id="search-date-arrival"
+                        />
+                        <p className="search-date-description">Select Trip Dates</p>
+                    </div>
+                    <div className="search-date-wrapper">
+                        <label htmlFor="search-date-departure">Date Departure</label>
+                        <input 
+                            type="date"
+                            value={departure}
+                            name="departure"
+                            onChange={(event) => setDeparture(event.target.value)}
+                            id="search-date-departure"
+                        />
+                        <p className="search-date-description">Select Trip Dates</p>
+                    </div>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
         </div>
     )
 }
