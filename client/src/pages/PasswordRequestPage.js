d1a21f98b540225054e7102fc78ca59aa0e1a022
33JaredL <jmarc.lagman@gmail.com>
2023-11-08 21:41:27 -0800

    Add password recovery pages

---
+++ client/src/pages/PasswordRequestPage.js
@@ -0,0 +1,10 @@
+import React from "react"
+import PasswordRequest from "../components/passwordreset/PasswordRequest"
+
+const PasswordRequestPage = () => {
+    return (
+        <PasswordRequest/>
+    )
+}
+
+export default PasswordRequestPage
