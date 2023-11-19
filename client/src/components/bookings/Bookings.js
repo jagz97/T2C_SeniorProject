c7e7264d70e127257557add3ed6f99c7a06a5e27
33JaredL <jmarc.lagman@gmail.com>
2023-11-19 13:24:06 -0800

    Add get request

--- client/src/components/bookings/Bookings.js
+++ client/src/components/bookings/Bookings.js
@@ -5,27 +5,58 @@ import Row from 'react-bootstrap/Row'
 import Booking from "./Booking"
 import introImg from "../../images/pexels-asad-photo-maldives-1591373.jpg"
 
+import { api } from "../../api/axios"
+import useAuth from '../../hooks/useAuth'
 import "./Bookings.css"
 
 const Bookings = () => {
     const [ bookings, setBookings ] = useState([])
 
+    const { user } = useAuth()
 
     useEffect(() => {
-        console.log("Fetch user bookings set to state")
+        const fetchBookings = async () => {
+            try {
+                const headerOptions = {
+                    headers: {
+                        Authorization: `${user.accesstoken}`,
+                    }
+                }
+                const usrBookings = await api.get("/reservations",headerOptions)
+                setBookings(usrBookings.data)
+            }
+            catch(error) {
+                console.log(error)
+            }
+        }
+        fetchBookings()
     }, [])
+
     const handleCancel = () => {
         console.log("handle cancel.")
     }
-    const tempData = <Booking
-        imgSrc={introImg}
-        title= "Double Flat with 3 Rooms"
-        checkin= "12 Mar 2021"
-        duration= "3 days"
-        guests= "4 Adults"
-        price= "1000"
-        handleCancel={handleCancel}
-    />
+
+    // const tempData = <Booking
+    //     imgSrc={introImg}
+    //     title= "Double Flat with 3 Rooms"
+    //     checkin= "12 Mar 2021"
+    //     duration= "3 days"
+    //     guests= "4 Adults"
+    //     price= "1000"
+    //     handleCancel={handleCancel}
+    // />
+
+    // const usrBookings = bookings.map((booking) => (
+    //     <Booking
+    //         imgSrc={}
+    //         title = {} 
+    //         checkin = {} 
+    //         duration = {} 
+    //         guests = {} 
+    //         price = {} 
+    //         handleCancel={handleCancel}
+    //     />
+    // ))
     
     return (
         <Container fluid>
@@ -44,19 +75,19 @@ const Bookings = () => {
                         </div>
             
                         <div className="container-bookings">
-                            {/* {
+                            {
                                 bookings.length === 0 ? 
                                     <p className="text-muted text-center mt-5">No Upcoming Bookings.</p>
                                     :
                                     {
                                         bookings
                                     }
-                            } */}
+                            }
+                            {/* {tempData}
                             {tempData}
                             {tempData}
                             {tempData}
-                            {tempData}
-                            {tempData}
+                            {tempData} */}
                         </div>
                     </div>
                 </Col>
