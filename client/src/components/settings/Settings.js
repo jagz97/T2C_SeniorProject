80b36e1f4ce331bf585709ad33d5ab365ecb8cdd
33JaredL <jmarc.lagman@gmail.com>
2023-11-26 12:18:53 -0800

    Fix eslint warnings

--- client/src/components/settings/Settings.js
+++ client/src/components/settings/Settings.js
@@ -131,7 +131,7 @@ const Settings = () => {
             }
         }
         fetchProfile()
-    }, []) 
+    }, [user.accesstoken]) 
 
     const handleSave = async () => {
         
--- client/src/components/settings/ToggleButton.js
+++ client/src/components/settings/ToggleButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import "./ToggleButton.css"
 
 /** Avatar Component Props
